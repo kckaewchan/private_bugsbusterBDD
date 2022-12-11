489dce69b82d59eab217cc3ea7873aa5e0289527
Chuleeporn <kckaewchan@gmail.com>
2022-12-11 14:21:19 -0500

    add for Jenkins

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "step_definitions",
         stepNotifications = true,
         dryRun = false,
-        tags = "@ASB-16"
+        tags = "@regression"
 )
 public class CukesRunner {
 }
